2862e3e7693babccdbb0494e70e048ff8496e2c4
Jagannadh <shanmuk.mir@gmail.com>
2014-03-10 00:21:23 +0530

    Indentation error fixed in 'js/background.js'

--- js/background.js
+++ js/background.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){	
 
-	var Domain = "https://channeli.in";
-	var Host = "channeli.in";
+		var Domain = "https://channeli.in";
+		var Host = "channeli.in";
 
-	var getDomainName = function (href) {
-    var l = document.createElement("a");
-    l.href = href;
-    return l.hostname;
-  }
+		var getDomainName = function (href) {
+		var l = document.createElement("a");
+		l.href = href;
+		return l.hostname;
+		}
 
-	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		var tab_url = tab.url;
-		var domain = getDomainName(tab_url);
-		if (tab_url !== undefined && changeInfo.status == "complete") {
+		chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+			var tab_url = tab.url;
+			var domain = getDomainName(tab_url);
+			if (tab_url !== undefined && changeInfo.status == "complete") {
 			if(domain == Host) {
-				var url = Domain + "/check-session/";
-  			$.post(url, function(res){
-    			if(res.msg == "YES") {
-						chrome.browserAction.setIcon({path: "../images/icon_active.png"});	
-					}
-					else if(res.msg == "NO") {
-						chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
-					}
+			var url = Domain + "/check-session/";
+			$.post(url, function(res){
+				if(res.msg == "YES") {
+				chrome.browserAction.setIcon({path: "../images/icon_active.png"});	
+				}
+				else if(res.msg == "NO") {
+				chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
+				}
 				});
 			}
-		}
-  }); 
+			}
+			}); 
 
 });
