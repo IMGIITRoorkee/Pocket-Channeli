c155a876327790064650d68d2e0b68b9e2e1ff92
Jagannath93 <shanmuk.mir@gmail.com>
2014-03-10 13:15:36 +0530

    Handled status updation on app load or refresh

--- js/background.js
+++ js/background.js
@@ -1,30 +1,36 @@
-$(document).ready(function(){	
+/* This script runs in the background from when the app is loaded */
 
-		var Domain = "https://channeli.in";
-		var Host = "channeli.in";
+var Domain = "https://channeli.in";
+var Host = "channeli.in";
 
-		var getDomainName = function (href) {
-		var l = document.createElement("a");
-		l.href = href;
-		return l.hostname;
-		}
+var getDomainName = function (href) {
+  var l = document.createElement("a");
+  l.href = href;
+  return l.hostname;
+}
 
-		chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-			var tab_url = tab.url;
-			var domain = getDomainName(tab_url);
-			if (tab_url !== undefined && changeInfo.status == "complete") {
-			if(domain == Host) {
-			var url = Domain + "/check-session/";
-			$.post(url, function(res){
-				if(res.msg == "YES") {
-				chrome.browserAction.setIcon({path: "../images/icon_active.png"});	
-				}
-				else if(res.msg == "NO") {
-				chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
-				}
-				});
-			}
-			}
-			}); 
+var checkSession = function() {
+  var url = Domain + "/check-session/";
+  $.post(url, function(res){
+    if(res.msg == "YES") {
+      chrome.browserAction.setIcon({path: "../images/icon_active.png"});	
+    }
+    else if(res.msg == "NO") {
+      chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
+    }
+  });
+}
 
-});
+// This is to handle the status updation when the app is "loaded" or "refreshed". 
+checkSession(); 
+
+/* Updates the status especially when the 'channeli.in' tabs are updated */
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  var tab_url = tab.url;
+  var domain = getDomainName(tab_url);
+  if (tab_url !== undefined && changeInfo.status == "complete") {
+    if(domain == Host) {
+      checkSession();
+    }
+  }
+}); 
--- js/popup.js
+++ js/popup.js
@@ -2,18 +2,13 @@ $(document).ready(function(){
 
   var Domain = "https://channeli.in";
   var Host = "channeli.in"
-  
+
+  /* Extracts Domain name from the url */
   var getDomainName = function (href) {
     var l = document.createElement("a");
     l.href = href;
     return l.hostname;
   }
-  
-  /*
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  	alert(changeInfo.url);
-  });
-  */  
 
   var url = Domain + "/check-session/"; 
  
@@ -55,100 +50,99 @@ $(document).ready(function(){
     }
   });
   
-$("#logout_btn").on("click", function(){
-  var url = Domain + "/logout-user/";
-  $.post(url, function(res){
-    if(res.msg == "OK")
-    {
-      $("#message").hide();
-      $("#user_box").hide();
-      $("#login").show();
-      $("#main").show();
-      $("#username_field").val('');
-      $("#password_field").val('');
+  $("#logout_btn").on("click", function(){
+    var url = Domain + "/logout-user/";
+    $.post(url, function(res){
+      if(res.msg == "OK")
+      {
+        $("#message").hide();
+        $("#user_box").hide();
+        $("#login").show();
+        $("#main").show();
+        $("#username_field").val('');
+        $("#password_field").val('');
 
-      chrome.tabs.query({}, function (tabs) {
-        var _tabs = [];
-        for (var i = 0; i < tabs.length; i++) {
-          if(getDomainName(tabs[i].url) == Host)
-          {
-            _tabs.push(tabs[i].id);
+        chrome.tabs.query({}, function (tabs) {
+          var _tabs = [];
+          for (var i = 0; i < tabs.length; i++) {
+            if(getDomainName(tabs[i].url) == Host)
+            {
+              _tabs.push(tabs[i].id);
+            }
           }
-        }
-        for(var j = 0; j < _tabs.length; j++)
-        {
-          chrome.tabs.reload(_tabs[j]);
-        }
-      });
-      chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
-    }
-    else if(res.msg == "FAILURE")
-    {
-      $("#message").hide();
-      $("#user_box").hide();
-      $("#login").show();
-      $("#main").show();
-    }
-    else{
-      $("#login").hide();
-      $("#user_box").hide();
-      $("#message").show();
-      var msg = "Error occured!";
-      $("#message").html("<p>" + msg + "</p>");
-      $("#main").show();
-    }
-  });
-});
-
-$("#login_form").submit(function(e){
-  e.preventDefault();
-  var url = Domain + "/check-login/";
-  var data = $("#login_form").serialize();
-  $.post(url, data, function(res){
-    if(res.msg == "YES")
-    {
-      $("#login").hide();
-      $("#message").hide();
-    	$("#user_box").show();
-    	$("#profile_box").html("<div id='profile_pic_box'><img src='"+ Domain + res.photo + "' alt='pic' class='profile_pic'/></div>"+ 
-          "<div id='user_name_box'><p id='user_name'>" + res._name + "</p> <p id='user_info'>" + res.info + "</p>");
-      $("#main").show();
-     
-      chrome.tabs.query({}, function (tabs) {
-        var _tabs = [];
-        for (var i = 0; i < tabs.length; i++) {
-          if(getDomainName(tabs[i].url) == Host)
+          for(var j = 0; j < _tabs.length; j++)
           {
-            _tabs.push(tabs[i].id);
+            chrome.tabs.reload(_tabs[j]);
           }
-        }
-        for(var j = 0; j < _tabs.length; j++)
-        {
-          chrome.tabs.reload(_tabs[j]);
-        }
-      });
-      chrome.browserAction.setIcon({path: "../images/icon_active.png"});
-    }
-    else if(res.msg == "NO")
-    {
-      $("#user_box").hide();
-      var msg = "Invalid Username or Password!";
-      $("#message").show();
-      $("#message").html("<p>" + msg + "</p>");
-      $("#login").show();
-      $("#main").show();
-    }
-    else if(res.msg == "FAILURE")
-    {
-      $("#user_box").hide();
-      var msg = "Invalid request!";
-      $("#message").show();
-      $("#message").html("<p>" + msg + "</p>");
-      $("#login").show();
-      $("#main").show();
-    }
+        });
+        chrome.browserAction.setIcon({path: "../images/icon_inactive.png"});
+      }
+      else if(res.msg == "FAILURE")
+      {
+        $("#message").hide();
+        $("#user_box").hide();
+        $("#login").show();
+        $("#main").show();
+      }
+      else{
+        $("#login").hide();
+        $("#user_box").hide();
+        $("#message").show();
+        var msg = "Error occured!";
+        $("#message").html("<p>" + msg + "</p>");
+        $("#main").show();
+      }
+    });
   });
-});
 
+  $("#login_form").submit(function(e){
+    e.preventDefault();
+    var url = Domain + "/check-login/";
+    var data = $("#login_form").serialize();
+    $.post(url, data, function(res){
+      if(res.msg == "YES")
+      {
+        $("#login").hide();
+        $("#message").hide();
+        $("#user_box").show();
+        $("#profile_box").html("<div id='profile_pic_box'><img src='"+ Domain + res.photo + "' alt='pic' class='profile_pic'/></div>"+ 
+            "<div id='user_name_box'><p id='user_name'>" + res._name + "</p> <p id='user_info'>" + res.info + "</p>");
+        $("#main").show();
+       
+        chrome.tabs.query({}, function (tabs) {
+          var _tabs = [];
+          for (var i = 0; i < tabs.length; i++) {
+            if(getDomainName(tabs[i].url) == Host)
+            {
+              _tabs.push(tabs[i].id);
+            }
+          }
+          for(var j = 0; j < _tabs.length; j++)
+          {
+            chrome.tabs.reload(_tabs[j]);
+          }
+        });
+        chrome.browserAction.setIcon({path: "../images/icon_active.png"});
+      }
+      else if(res.msg == "NO")
+      {
+        $("#user_box").hide();
+        var msg = "Invalid Username or Password!";
+        $("#message").show();
+        $("#message").html("<p>" + msg + "</p>");
+        $("#login").show();
+        $("#main").show();
+      }
+      else if(res.msg == "FAILURE")
+      {
+        $("#user_box").hide();
+        var msg = "Invalid request!";
+        $("#message").show();
+        $("#message").html("<p>" + msg + "</p>");
+        $("#login").show();
+        $("#main").show();
+      }
+    });
+  });
 });
 
